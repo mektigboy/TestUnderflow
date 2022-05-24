c612898cac9e52640ac4600d3486f5ef3798c52b
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-23 23:21:33 -0500

    Update TestUnderflow.sol

--- TestUndeflow.sol
+++ TestUndeflow.sol
@@ -2,14 +2,14 @@
 pragma solidity ^ 0.8.14;
 
 contract SafeMath {
-  function testUnderflow() public pure returns (uint) {
-    uint x = 0;
+  function testUnderflow() public pure returns (uint8) {
+    uint8 x = 0;
     x--;
     return x;
   }
 
-  function testUncheckedUnderflow() public pure returns (uint) {
-    uint x = 0;
+  function testUncheckedUnderflow() public pure returns (uint8) {
+    uint8 x = 0;
     unchecked { x--; }
     return x;
   }
